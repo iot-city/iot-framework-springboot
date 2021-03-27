4607682d49be6f93ecce54f8f7aeb22a7ec83def
ardon <ind_nc@163.com>
2021-03-27 23:21:17 +0800

    Project initialization.

---
+++ iot-framework-spring-boot-starter/src/main/java/org/iotcity/iot/framework/spring/boot/starter/IotFrameworkSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.iotcity.iot.framework.spring.boot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotFrameworkSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotFrameworkSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ iot-framework-spring-boot-starter/src/test/java/org/iotcity/iot/framework/spring/boot/starter/IotFrameworkSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iotcity.iot.framework.spring.boot.starter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotFrameworkSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
